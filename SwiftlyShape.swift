a1849ee5c0928434883f2f6d98014b2ab1e3dd68
Cupboard <66140148+DrinkCupboard@users.noreply.github.com>
2021-01-09 17:43:19 +0000

    Added Mound - The first shape 🎉
    - Added Mound Shape
    It's a semi-circle of sorts, but technically not. It would look like the letter "D", if it were laying on its side (flat edge down).

---
+++ SwiftlyShape.swift
@@ -0,0 +1,28 @@
+//
+//  SwiftlyShape.swift
+//  iOS
+//
+//  Created by Cupboard Coffee Limited
+//
+
+import SwiftUI
+
+struct Mound: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addQuadCurve(to: CGPoint(x: rect.midX, y: rect.minY), control: CGPoint(x: rect.minX, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.maxY), control: CGPoint(x: rect.maxX, y: rect.minY))
+        return path
+    }
+}
+
+
+struct SwiftlyShape_Previews: PreviewProvider {
+    static var previews: some View {
+        Mound()
+            .frame(width: 300, height: 200)
+            .background(Color(.black))
+    }
+}
